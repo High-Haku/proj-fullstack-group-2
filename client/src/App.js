97b0dc3b6b8800c6590d3a1bedc99db63c82c0c4
doloshabell <habelldol@gmail.com>
2022-04-18 11:33:28 +0700

    add rewards

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,12 @@ import Register from "./components/Register"
 import 'antd/dist/antd.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Contents from './components/Contents';
+import Rewards from './components/Rewards'
 
 function App() {
   return (
-    <div className="App">
-      <Contents/>
+    <div>
+      <Rewards />
     </div>
   );
 }
