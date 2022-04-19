63523aee3dd2633a5fc46ef077ed08ee8d4405c3
StvJo <sjon10422@gmail.com>
2022-04-19 09:35:51 +0700

    edit content detail files

--- client/src/components/ContentDetail.js
+++ client/src/components/ContentDetail.js
@@ -1,51 +1,62 @@
 import React from "react";
 import { Col, Container, Row, Button, Form } from "react-bootstrap";
 import "./ContentDetail.css";
+import VideoPlayerFunction from "../Config/CloudinaryVideoPLayer";
 
 function ContentDetail() {
+  const videoOptions = {
+    cloudName: "jovitoaddo",
+    publicId: "cptvg3z5b5mcjs1gquap",
+  };
+
   return (
     <div>
       <Container className="bootdey mt-5">
-        <section className="panel">
-          <Row>
-            <Col className="col-md-6">
-              <div class="pro-img-details">
-                <video
-                  src="https://v16-webapp.tiktok.com/375add6057d6f5c8122797aa98ed6440/625d09eb/video/tos/useast2a/tos-useast2a-ve-0068c003/e1ea3805dfef48ceb4988f9d2a89563f/?a=1988&amp;br=2680&amp;bt=1340&amp;cd=0%7C0%7C1%7C0&amp;ch=0&amp;cr=0&amp;cs=0&amp;cv=1&amp;dr=0&amp;ds=3&amp;er=&amp;ft=eXd.6Hy2Myq8Z77Rwwe2NVYQml7Gb&amp;l=202204180048160102440261890F54617D&amp;lr=tiktok_m&amp;mime_type=video_mp4&amp;net=0&amp;pl=0&amp;qs=0&amp;rc=ajlnNWt5eTVkeTMzaDczM0ApaDc6Z2c2Zjw7NzZnOTY8ZmcyajI1ZjZqY19fLS1iMTZzc180Mi5hY2FjNS01NTQ2XzY6Yw%3D%3D&amp;vl=&amp;vr="
-                  playsinline=""
-                  autoplay=""
-                ></video>
-              </div>
-            </Col>
-            <Col className="col-md-6">
-              <h1>Project Title</h1>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Age
-                nunc isti doceant, vel tu potius quis enim ista melius? Duo enim
-                genera quae erant, fecit tria. Te enim iudicem aequum puto, modo
-                quae dicat ille bene noris. Idemne potest esse dies saepius, qui
-                semel fuit?{" "}
-              </p>
-              <div className="mt-3">
-                <Button style={{ marginLeft: 20 }}>Like</Button>
-                <Button style={{ marginLeft: 20 }}>Share</Button>
-                <Button className="btn-pitch" style={{ marginLeft: 20 }}>
-                  Pitch
-                </Button>
-              </div>
-              <Form className="mt-5">
-                <Form.Group
-                  className="mb-3"
-                  controlId="exampleForm.ControlTextarea1"
-                >
-                  <Form.Label>Comment</Form.Label>
-                  <Form.Control as="textarea" rows={3} placeholder="Input your comment" />
-                </Form.Group>
-              </Form>
-              <Button className="btn-send">Send</Button>
-            </Col>
-          </Row>
-        </section>
+        <Row className="row-cols-8">
+          <Col id="left-col" className="col-md-5">
+            <VideoPlayerFunction className="vid" options={videoOptions} />
+          </Col>
+          <Col id="mid-col">
+            <div id="btn-group" className="col-md-1 order-second">
+              <Button className="mb-2">Like</Button>
+              <Button className="mb-2">Share</Button>
+              <Button className="mb-2">Pitch</Button>
+            </div>
+          </Col>
+          <Col id="right-col" className="col-md-6 order-third">
+            <h1>Project Title</h1>
+            <br />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Age nunc
+              isti doceant, vel tu potius quis enim ista melius? Duo enim genera
+              quae erant, fecit tria. Te enim iudicem aequum puto, modo quae
+              dicat ille bene noris. Idemne potest esse dies saepius, qui semel
+              fuit?{" "}
+            </p>
+            <Form className="mt-5">
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlTextarea1"
+              >
+                <Form.Label>Comment</Form.Label>
+                <Form.Control
+                  id="comment-box"
+                  as="textarea"
+                  rows={3}
+                  placeholder="Input your comment"
+                />
+              </Form.Group>
+            </Form>
+            <Button className="btn-send">Send</Button>
+            <br />
+            <br />
+            <br />
+            <h4>Comments Section :</h4>
+            <br />
+            <br />
+            <br />
+          </Col>
+        </Row>
       </Container>
     </div>
   );
