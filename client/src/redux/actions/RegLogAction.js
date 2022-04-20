9b9b1927a9926a5a10782cf1133c92cb912335f3
JovitoAddo <jovitoaddo@gmail.com>
2022-04-20 13:43:16 +0700

    merge

--- client/src/redux/actions/RegLogAction.js
+++ client/src/redux/actions/RegLogAction.js
@@ -1,18 +1,6 @@
 import axios from "axios";
-// export const POST_LOGIN = 'POST_LOGIN'
 export const POST_REGISTER = 'POST_REGISTER'
 export const GET_USERS = 'GET_USERS'
-
-// export const getUsers = () => async (dispatch) => {
-//     try {
-//         const { data } = await api.getAllContents();
-//         // console.log(data)
-//         dispatch({ type: GET_USERS, payload: data })
-//       } catch (error) {
-//         console.log(error.message);
-//       }
-
-// }
 
 export const postRegister = (data) => {
     return(dispatch) => {
@@ -51,39 +39,3 @@ export const postRegister = (data) => {
         })
     }
 }
-
-// export const postLogin = (data) => {
-//     return(dispatch) => {
-//         dispatch({
-//             type: POST_LOGIN,
-//             payload:{
-//                 loading: true,
-//                 data: false
-//             }
-//         })
-//         axios({
-//             method: "POST",
-//             url: "http://localhost:5000/login",
-//             data: data
-//         })
-//         .then((response)=>{
-//             dispatch({
-//                 type : POST_LOGIN,
-//                 payload:{
-//                     loading: false,
-//                     data: response.data
-//                 }
-//             })
-//         })
-//         .catch((error)=>{
-//             dispatch({
-//                 type: POST_REGISTER,
-//                 payload: {
-//                     loading: false,
-//                     data: false,
-//                     error
-//                 }
-//             })
-//         })
-//     }
-// }
